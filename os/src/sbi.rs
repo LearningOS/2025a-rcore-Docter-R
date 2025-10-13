bb390c3b5361a433e6bd84d6089d2b62bc85d7c9
Docter-R <3226440754@qq.com>
2025-10-13 16:43:08 +0800

    update

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -4,6 +4,7 @@ use core::arch::asm;
 
 const SBI_CONSOLE_PUTCHAR: usize = 1;
 
+
 /// general sbi call
 #[inline(always)]
 fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
