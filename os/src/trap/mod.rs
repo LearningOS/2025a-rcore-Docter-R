e5c8cd36d485e203347dc96cb8ed5f1bee35bae1
Docter-R <3226440754@qq.com>
2026-01-07 08:39:00 +0800

    update

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -41,14 +41,17 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     let scause = scause::read(); // get trap cause
     let stval = stval::read(); // get extra value
     match scause.cause() {
+        //  处理系统调用 (UserEnvCall)
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
+        // 处理访存错误 (StoreFault / StorePageFault)
         Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
             println!("[kernel] PageFault in application, kernel killed it.");
             run_next_app();
         }
+        // 处理非法指令 (IllegalInstruction)
         Trap::Exception(Exception::IllegalInstruction) => {
             println!("[kernel] IllegalInstruction in application, kernel killed it.");
             run_next_app();
